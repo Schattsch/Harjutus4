ecaf9ff8b66acfacc2a7d74cadeff0e4f293cf38
Schattsch <karelsch123@gmail.com>
2023-10-19 13:34:28 +0300

    Exercise 4.6: Container

---
+++ src/com/karel/program/Container.java
@@ -0,0 +1,33 @@
+package com.karel.program;
+
+import java.util.ArrayList;
+
+public class Container {
+    private final int maximumWeight;
+    private final ArrayList<Suitcase> suitcases;
+
+    public Container(int maximumWeight) {
+        this.maximumWeight = maximumWeight;
+        this.suitcases = new ArrayList<>();
+    }
+
+    public void addSuitcase(Suitcase suitcase) {
+        if (totalWeight() + suitcase.totalWeight() <= maximumWeight) {
+            suitcases.add(suitcase);
+        }
+    }
+
+    public String toString() {
+        int numSuitcases = suitcases.size();
+        int totalWeight = totalWeight();
+        return numSuitcases + " suitcase" + (numSuitcases != 1 ? "s" : "") + " (" + totalWeight + " kg)";
+    }
+
+    private int totalWeight() {
+        int total = 0;
+        for (Suitcase suitcase : suitcases) {
+            total += suitcase.totalWeight();
+        }
+        return total;
+    }
+}
